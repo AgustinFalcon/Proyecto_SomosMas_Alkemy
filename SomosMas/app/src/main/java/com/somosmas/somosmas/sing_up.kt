016c79f46a898595b57e44596d37ca930b38dca9
AgustinFalcon <agustinrobertofalcon@gmail.com>
2021-07-29 10:30:01 -0300

    sing up solo layout completado

--- SomosMas/app/src/main/java/com/somosmas/somosmas/sing_up.kt
+++ SomosMas/app/src/main/java/com/somosmas/somosmas/sing_up.kt
@@ -2,6 +2,7 @@ package com.somosmas.somosmas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
@@ -17,6 +18,8 @@ class sing_up : AppCompatActivity() {
         val txt_email = findViewById<TextInputLayout>(R.id.txt_email)
         val txt_password = findViewById<TextInputLayout>(R.id.txt_password)
         val txt_confirm_password = findViewById<TextInputLayout>(R.id.txt_confirm_password)
+        val btn_singUp = findViewById<Button>(R.id.btn_singUp)
+
 
 
     }
