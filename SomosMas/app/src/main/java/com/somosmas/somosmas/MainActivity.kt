2d77accd824d7a4e649ed7841ac8d79bf272a78d
Jeremias Gonzalez <jeregonzalez020@gmail.com>
2021-08-02 12:36:54 +0000

    Merged in retrofit (pull request #3)
    Retrofit
    
    * creacion de la vista Login
    
    * creacion de la vista Login
    
    * creacion de la vista Login
    
    * xml dimens creado con las respectivas dimensiones genericas
    
    * se dejaron las tareas correspondientes al ticket Login
    
    * implementacion de la libreria retrofit y creacion de la variable de entorno URL
    
    * Merge branch 'develop' of https://bitbucket.org/alkemy-dev/t50-project into retrofit
    
    * implementacion de retrofit y creacion de la funcion que llama a la api
    
    * nombre de las variables traducidads a ingles
    
    * crea cliente para Retrofit
    
    Approved-by: Andres David Serrano

--- SomosMas/app/src/main/java/com/somosmas/somosmas/MainActivity.kt
+++ SomosMas/app/src/main/java/com/somosmas/somosmas/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val retrofitConnection = RetrofitClient().getRetrofit()
         setContentView(R.layout.activity_sing_up)
     }
 
---
+++ SomosMas/app/src/main/java/com/somosmas/somosmas/RetrofitClient.kt
@@ -0,0 +1,19 @@
+package com.somosmas.somosmas
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitClient {
+
+    //this companion objet uses the library retrofit for call the APIService
+    companion object {
+        private val URL: String = "http://ongapi.alkemy.org/api/"
+        val builderRetrofit =
+            Retrofit.Builder().baseUrl("$URL").addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+    }
+    fun getRetrofit(): Retrofit {
+        return RetrofitClient.builderRetrofit
+    }
+}
