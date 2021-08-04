c108eb573c047ad5baeb436f69121b1954199d9e
AgustinFalcon <agustinrobertofalcon@gmail.com>
2021-08-04 11:16:57 -0300

    campos verificados correctamente, falta agregar diseño de errores

--- SomosMas/app/src/main/java/com/somosmas/somosmas/SingUpViewModel.kt
+++ SomosMas/app/src/main/java/com/somosmas/somosmas/SingUpViewModel.kt
@@ -13,17 +13,19 @@ class SingUpViewModel: ViewModel() {
     private var fieldsOk: Boolean = true
     val passwordFormat = Pattern.compile("(?=.*[0-9])") //Indica que por lo menos debe haber un numero
 
+    val numeros = arrayListOf(1,2,3,4,5,6,7,8,9)
+
     fun validateFields(name: String, email: String, pw1: String, pw2: String){
 
         if(name.isEmpty()){
             _viewState.value = SingUpViewStates.FieldError
             fieldsOk = false
         }
-        if(email.isEmpty() /*|| PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()*/){
+        if(email.isEmpty() || !PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()){
             _viewState.value = SingUpViewStates.FieldError
             fieldsOk = false
         }
-        if(pw1.isEmpty() /*|| passwordFormat.matcher(pw1).matches()*/){
+        if(pw1.isEmpty() || !pw1.contains("\\d+(?:\\.\\d+)?".toRegex()) ){
             _viewState.value = SingUpViewStates.FieldError
             fieldsOk = false
         }
@@ -36,6 +38,7 @@ class SingUpViewModel: ViewModel() {
         }
     }
 }
+
 
 
 sealed class SingUpViewStates{
