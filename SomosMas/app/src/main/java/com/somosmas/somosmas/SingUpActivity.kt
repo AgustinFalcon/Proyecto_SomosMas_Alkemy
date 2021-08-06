bf2faeb1007d0bafa978077309d206a891e5cad7
Agustin Falcon <agustinrobertofalcon@gmail.com>
2021-08-06 12:56:13 +0000

    Merged in SingUp (pull request #2)
    SingUp
    
    * ultimo commit
    
    * Merge branch 'develop' of https://bitbucket.org/alkemy-dev/t50-project into SingUp
    
     Conflicts:
    	SomosMas/app/build.gradle
    	SomosMas/app/src/main/res/values/colors.xml
    
    * cambio a cammel case
    
    * Merged in retrofit (pull request #3)
    
    Retrofit
    
    * creacion de la vista Login
    
    * creacion de la vista Login
    
    * creacion de la vista Login
    
    * xml dimens creado con las respectivas dimensiones genericas
    
    * se dejaron las tareas correspondientes al ticket Login
    
    * implementacion de la libreria retrofit y creacion de la variable de entorno URL
    
    * Merge branch 'develop' of https://bitbucket.org/alkemy-dev/t50-project into retrofit
    
    * implementacion de retrofit y creacion de la funcion que llama a la api
    
    * nombre de las variables traducidads a ingles
    
    * crea cliente para Retrofit
    
    Approved-by: Andres David Serrano
    
    * campos corregidos
    
    * Merge branch 'develop' of https://bitbucket.org/alkemy-dev/t50-project into SingUp
    
     Conflicts:
    	SomosMas/app/src/main/java/com/somosmas/somosmas/SingUpActivity.kt
    	SomosMas/app/src/main/res/layout/activity_sing_up.xml
    
    * fix singup branch, recover reverted changes
    
    
    Approved-by: Andres David Serrano

--- SomosMas/app/src/main/java/com/somosmas/somosmas/SingUpActivity.kt
+++ SomosMas/app/src/main/java/com/somosmas/somosmas/SingUpActivity.kt
@@ -1,47 +1,18 @@
 package com.somosmas.somosmas
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.lifecycle.ViewModelProvider
-import com.somosmas.somosmas.databinding.ActivitySingUpBinding
-
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class SingUpActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivitySingUpBinding
-    private lateinit var singUpViewModel : SingUpViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySingUpBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val name = binding.txtName
-        val email = binding.txtEmail
-        val password = binding.txtPassword
-        val confirm_password = binding.txtConfirmPassword
-
-        singUpViewModel = ViewModelProvider(this).get(SingUpViewModel::class.java)
-        singUpViewModel.viewStates.observe(this,::handleViewState)
-
-        binding.btnSingUp.setOnClickListener {
-            singUpViewModel.validateFields(name.text.toString(), email.text.toString(), password.text.toString(), confirm_password.text.toString())
-        }
+        setContentView(R.layout.activity_sing_up)
     }
-
-    private fun handleViewState(viewState: SingUpViewStates){
-        when(viewState){
-            is SingUpViewStates.FieldErrorName -> binding.InputName.error = " "
-            is SingUpViewStates.FieldErrorEmail -> binding.InputEmail.error = " "
-            is SingUpViewStates.FieldErrorPassword -> binding.InputPassword.error = " "
-            is SingUpViewStates.FieldErrorConfirmPassword -> binding.InputConfirmPassword.error = "Passwords are not the same"
-            is SingUpViewStates.FieldSucces -> toast(this,"Succes") //INTENT A HOME
-        }
-    }
-
-    fun Context.toast(context: Context = applicationContext, message: String, duration: Int = Toast.LENGTH_SHORT){
-        Toast.makeText(context, message, duration).show()
-    }
+    
 }
