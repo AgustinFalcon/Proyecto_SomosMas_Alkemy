6df0063d9a84d1ef1b4ee09eb73d3d2bcff8b53a
Jeremias Gonzalez <jeregonzalez020@gmail.com>
2021-08-06 16:32:35 +0000

    Merged in jgBranch (pull request #7)
    JgBranch
    
    * xml dimens creado con las respectivas dimensiones genericas
    
    * se dejaron las tareas correspondientes al ticket Login
    
    * archivo dimens
    
    * toolbar eliminada y dimensiones de los botones agregadas al archivo
    
    * apply constraintlayout in the design
    
    * ticket splash removed
    
    * Merge branch 'develop' of https://bitbucket.org/alkemy-dev/t50-project into jgBranch
    
     Conflicts:
    	SomosMas/app/build.gradle
    	SomosMas/app/src/main/java/com/somosmas/somosmas/MainActivity.kt
    	SomosMas/app/src/main/res/layout/activity_main.xml
    
    * removed splash theme
    
    * dimens fixed
    
    * margins start/end removed
    
    
    Approved-by: Andres David Serrano

--- SomosMas/app/src/main/java/com/somosmas/somosmas/MainActivity.kt
+++ SomosMas/app/src/main/java/com/somosmas/somosmas/MainActivity.kt
@@ -3,21 +3,10 @@ package com.somosmas.somosmas
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val retrofitConnection = RetrofitClient().getRetrofit()
-
-        val btn_singUp = findViewById<Button>(R.id.btnSingUp)
-
-        btn_singUp.setOnClickListener {
-            val intent = Intent(this,SingUpActivity::class.java)
-            startActivity(intent)
-        }
     }
-
 }
