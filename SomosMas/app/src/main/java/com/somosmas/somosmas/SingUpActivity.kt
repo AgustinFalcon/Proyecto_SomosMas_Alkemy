8048064da6c796cd8ded9249c5487b65ec45db7a
AgustinFalcon <agustinrobertofalcon@gmail.com>
2021-08-10 14:40:16 -0300

    ultimos cambios aplicados

--- SomosMas/app/src/main/java/com/somosmas/somosmas/SingUpActivity.kt
+++ SomosMas/app/src/main/java/com/somosmas/somosmas/SingUpActivity.kt
@@ -4,9 +4,9 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.ViewModelProvider
 import com.somosmas.somosmas.databinding.ActivitySingUpBinding
-import java.util.zip.Inflater
 
 
 class SingUpActivity : AppCompatActivity() {
@@ -20,33 +20,34 @@ class SingUpActivity : AppCompatActivity() {
         binding = ActivitySingUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val name = binding.txtName
-        val email = binding.txtEmail
-        val password = binding.txtPassword
-        val confirm_password = binding.txtConfirmPassword
-
         singUpViewModel = ViewModelProvider(this).get(SingUpViewModel::class.java)
         singUpViewModel.viewStates.observe(this, ::handleViewState)
 
 
-            binding.btnSingUp.setOnClickListener {
-                singUpViewModel.validateFields(name.text.toString(), email.text.toString(), password.text.toString(), confirm_password.text.toString()
-                )
+            binding.btnSingUp.setOnClickListener{
+                singUpViewModel.validateFields(
+                    binding.txtName.text.toString(),
+                    binding.txtEmail.text.toString(),
+                    binding.txtPassword.text.toString(),
+                    binding.txtConfirmPassword.text.toString())
             }
     }
 
 
-        private fun handleViewState(viewState: SingUpViewStates){
-            var btn_singUp = binding.btnSingUp
-            when(viewState){
-                is SingUpViewStates.FieldErrorName -> binding.InputName.error = " "
-                is SingUpViewStates.FieldErrorEmail -> binding.InputEmail.error = " "
-                is SingUpViewStates.FieldErrorPassword -> binding.InputPassword.error = " "
-                is SingUpViewStates.FieldErrorConfirmPassword -> binding.InputConfirmPassword.error = " "
-                is SingUpViewStates.FieldErrorConfirmPasswordMessage -> binding.InputConfirmPassword.error = "Passwords are not the same"
-                is SingUpViewStates.FieldSucces -> toast(this,"Succes") //INTENT A HOME
-            }
+    private fun handleViewState(viewState: SingUpViewStates){
+        when(viewState){
+            is SingUpViewStates.FieldErrorName -> binding.InputName.error = " "
+            is SingUpViewStates.SuccesName -> binding.InputName.error = null
+            is SingUpViewStates.FieldErrorEmail -> binding.InputEmail.error = " "
+            is SingUpViewStates.SuccesEmail -> binding.InputEmail.error = null
+            is SingUpViewStates.FieldErrorPassword -> binding.InputPassword.error = " "
+            is SingUpViewStates.SuccesPassword -> binding.InputPassword.error = null
+            is SingUpViewStates.FieldErrorConfirmPassword -> binding.InputConfirmPassword.error = " "
+            is SingUpViewStates.FieldErrorConfirmPasswordMessage -> binding.InputConfirmPassword.error = "Passwords are not the same"
+            is SingUpViewStates.SuccesConfirmPassword -> binding.InputConfirmPassword.error = null
+            is SingUpViewStates.FieldSucces -> toast(this,"Succes") //INTENT A HOME
         }
+    }
 
 
         fun Context.toast(context: Context = applicationContext, message: String, duration: Int = Toast.LENGTH_SHORT){
--- SomosMas/app/src/main/java/com/somosmas/somosmas/SingUpViewModel.kt
+++ SomosMas/app/src/main/java/com/somosmas/somosmas/SingUpViewModel.kt
@@ -10,30 +10,41 @@ class SingUpViewModel: ViewModel() {
     private val _viewState = MutableLiveData<SingUpViewStates>()
     val viewStates : LiveData<SingUpViewStates>get() = _viewState
 
-    private var fieldsOk: Boolean = true
 
     fun validateFields(name: String, email: String, pw1: String, pw2: String){
+        var fieldsOk: Boolean = true
 
         if(name.isEmpty()){
             _viewState.value = SingUpViewStates.FieldErrorName
             fieldsOk = false
+        }else{
+            _viewState.value = SingUpViewStates.SuccesName
         }
         if(email.isEmpty() || !PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()){
             _viewState.value = SingUpViewStates.FieldErrorEmail
             fieldsOk = false
+        }else{
+            _viewState.value = SingUpViewStates.SuccesEmail
         }
         if(pw1.isEmpty() || !pw1.contains("\\d+(?:\\.\\d+)?".toRegex()) ){
             _viewState.value = SingUpViewStates.FieldErrorPassword
             fieldsOk = false
+        }else{
+            _viewState.value = SingUpViewStates.SuccesPassword
         }
+
         if (pw2.isEmpty()){
             _viewState.value = SingUpViewStates.FieldErrorConfirmPassword
             fieldsOk = false
+        }else{
+            _viewState.value = SingUpViewStates.SuccesConfirmPassword
         }
+
         if(pw1 != pw2){
             _viewState.value = SingUpViewStates.FieldErrorConfirmPasswordMessage
             fieldsOk = false
         }
+
         if(fieldsOk){
             _viewState.value = SingUpViewStates.FieldSucces
         }
@@ -42,9 +53,13 @@ class SingUpViewModel: ViewModel() {
 
 sealed class SingUpViewStates{
     object FieldErrorName: SingUpViewStates()
+    object SuccesName: SingUpViewStates()
     object FieldErrorEmail: SingUpViewStates()
+    object SuccesEmail: SingUpViewStates()
     object FieldErrorPassword: SingUpViewStates()
+    object SuccesPassword: SingUpViewStates()
     object FieldErrorConfirmPassword: SingUpViewStates()
     object FieldErrorConfirmPasswordMessage: SingUpViewStates()
+    object SuccesConfirmPassword: SingUpViewStates()
     object FieldSucces: SingUpViewStates()
 }
