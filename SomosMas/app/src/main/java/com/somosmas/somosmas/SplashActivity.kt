7a3f49a21ad51a8a3fbfdd68b05250a4b38e8959
Jeremias Gonzalez <jeregonzalez020@gmail.com>
2021-08-11 19:39:31 +0000

    Merged in timerBR (pull request #13)
    Ejecutar un timer que indique la duración en pantalla del Splash
    
    * timer implements in new branch
    
    
    Approved-by: Andres David Serrano

--- SomosMas/app/src/main/java/com/somosmas/somosmas/SplashActivity.kt
+++ SomosMas/app/src/main/java/com/somosmas/somosmas/SplashActivity.kt
@@ -3,12 +3,19 @@ package com.somosmas.somosmas
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+
 
 class SplashActivity : AppCompatActivity() {
+    private val SPLASH_TIME: Long = 5000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_SomosMas)
 
-        startActivity(Intent(this, MainActivity::class.java))
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME)
     }
 }
