377769c7eb61c5af8f4655dfda8d383e3554a85d
AgustinFalcon <agustinrobertofalcon@gmail.com>
2021-08-19 12:31:41 -0300

    strings no necesarios eliminados + action bar con el nombre indicado

--- SomosMas/app/src/main/java/com/somosmas/somosmas/home/ui/us/UsViewModel.kt
+++ SomosMas/app/src/main/java/com/somosmas/somosmas/home/ui/us/UsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class UsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is nosotros Fragment"
     }
     val text: LiveData<String> = _text
 }
